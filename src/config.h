e70494c31a232da2193c2bbb45872b178fcc234b
liyaFPV <lbkmzc942@gmail.com>
2026-03-17 14:16:51 +0500

    add sd

--- src/config.h
+++ src/config.h
@@ -1,4 +1,11 @@
-#define CS_PIN    10
-#define DC_PIN    9
-#define RST_PIN   8
-#define BUSY_PIN  7
+#define CS_PIN    11
+#define DC_PIN    10
+#define RST_PIN   9
+#define BUSY_PIN  8
+#define CLK_PIN   12  // SCK
+#define DIN_PIN   13  // MOSI
+
+#define SD_CS   3
+#define SD_MOSI 13
+#define SD_MISO 2
+#define SD_SCK  12
--- src/main.cpp
+++ src/main.cpp
@@ -2,25 +2,47 @@
 #include <Adafruit_GFX.h>
 #include <U8g2_for_Adafruit_GFX.h>
 #include "config.h"
+#include <SPI.h>
+#include "sd.h"
 
+// инициализация дисплея через hardware SPI
 GxEPD2_BW<GxEPD2_290_T94_V2, GxEPD2_290_T94_V2::HEIGHT> display(GxEPD2_290_T94_V2(CS_PIN, DC_PIN, RST_PIN, BUSY_PIN));
+
 U8G2_FOR_ADAFRUIT_GFX u8g2_for_adafruit;
 
-void elink_init(){
-display.init();
+void elink_init() {
+  // hardware SPI с кастомными пинами
+  SPI.begin(CLK_PIN, -1, DIN_PIN, CS_PIN); // SCK, MISO(-1), MOSI, CS
+
+  display.init();
   display.setRotation(1);
   display.fillScreen(GxEPD_WHITE);
+
   u8g2_for_adafruit.begin(display);
   u8g2_for_adafruit.setFont(u8g2_font_unifont_t_cyrillic);
   u8g2_for_adafruit.setForegroundColor(GxEPD_BLACK);
   u8g2_for_adafruit.setBackgroundColor(GxEPD_WHITE);
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
+
+  SPI.begin(12, 2, 13);
+
+  pinMode(CS_PIN, OUTPUT);
+  digitalWrite(CS_PIN, HIGH);
+
+  if (sd_init())
+  {
+    sd_info();
+  }
+
   elink_init();
-  u8g2_for_adafruit.setCursor(0, 10);  // координаты уже с учётом rotation
-  u8g2_for_adafruit.print("Привет, мир!");
+
+  u8g2_for_adafruit.setCursor(0,10);
+  u8g2_for_adafruit.print("Привет");
+
   display.display(true);
 }
 
