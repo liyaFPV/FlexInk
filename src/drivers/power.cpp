f293488a508e2cc6ffc9a666cb4c1e4e262d8c23
liyaFPV <lbkmzc942@gmail.com>
2026-04-17 22:55:46 +0500

    добавлено снижение чистаты cpu

--- src/drivers/power.cpp
+++ src/drivers/power.cpp
@@ -5,11 +5,13 @@ bool power_tick(){
 }
 
 void sleep(){
+    setCpuFrequencyMhz(80);
     elink_sleep();
     sd_end();
 }
 
 void wakeUp(){
+    setCpuFrequencyMhz(240);
     elink_wakeUp();
     sd_init();
 }
